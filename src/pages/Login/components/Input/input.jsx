e1bd7f793166d3c436a315476b63f9f47de939e9
geremias monsalve <geremiasmonsalve526@gmail.com>
2023-09-20 20:15:56 -0300

    Creando el fondo del login en css

--- src/pages/Login/components/Input/input.jsx
+++ src/pages/Login/components/Input/input.jsx
@@ -4,7 +4,7 @@ import './Input.css';
 const input = ({ attribute, handleChange, param}) => {
     return (
         <div>
-            <input id="" name="" placeholder="ingrese su usuario" type="text" onChange={} className=""/>
+            <input id="attribute.id" name="attribute.name" placeholder="ingrese su usuario" type="text" onChange={} className=""/>
             <input id="" name="" placeholder="ingrese su contraseña" type="password" onChange={} className=""/>
         </div>
     )
--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 import Title from "./components/Title/title";
 import Label from "./components/Label/Label";
 import input from "./components/input/input";
 
 const Login = () => {
+    const [ user, setUser ] = useState('');
+    const [ password, setPassword ] = useState('');
+
+    //Variable para almacenar usuario
+    function handleChange (name, value) {
+     if(name == 'usuario'){
+      setUser(value)
+     }else{
+        setPassword(value)
+     }
+    };
+    function handleSubmit(){
+        let account = { user, password }
+        if(account){
+            console.log('account:', account)
+        }
+    };
     return (
-        <div className="">
+        <div className="login-container">
            <Title text= "Hola bienvenidos a " />
            <Label text= "ingrese su usario" />
-           <Input />
+           <Input
+           attribute={{
+            id: 'usuario',
+            name: 'usuario',
+            type: 'text',
+            placeholder: 'Ingrese su usuario'
+           }} 
+           handleChange ={handleChange}
+           />
            <Label text= "Contraseña" />
-           <Input />
+           <Input 
+           attribute={{
+            id: 'contraseña',
+            name: 'contraseña',
+            type: 'password',
+            placeholder: 'Ingrese su Contraseña'
+           }}
+           handleChange={handleChange}
+           />
+           <button onClick={ (e) => handleSubmit }>
+            Ingresar
+           </button>
         </div>
     )
 };
