850973d6f4c8ff37dbe759c1a188899427700f95
Benjamin Sepulveda <bpancho05@gmail.com>
2023-11-14 18:02:14 -0300

    mostrarOcultarMenu

---
+++ src/menu/menu.js
@@ -0,0 +1,15 @@
+let menuVisible = false;
+//funcion que muestra o oculta el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    
+}
